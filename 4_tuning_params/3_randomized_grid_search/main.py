5b6f4a4166c972966f38562f1f2b0f31e67d205c
Saketh Katari <katari15045@iiitd.ac.in>
2018-08-09 17:41:26 +0530

    Adds Randomized Search CV

---
+++ 4_tuning_params/3_randomized_grid_search/main.py
@@ -0,0 +1,21 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+
+# Load Data
+iris = load_iris()
+X = iris.data
+y = iris.target
+print("Raw Data : " + str(X.shape) + " | " + str(y.shape))
+
+# Make Parametres
+params = {}
+params["n_neighbors"] = range(1, 31)
+params["weights"] = ["uniform", "distance"]
+
+# Perform Grid Search
+knn = KNeighborsClassifier()
+ran_sear = RandomizedSearchCV(knn, params, cv=10, scoring="accuracy", n_iter=10, n_jobs=-1)
+ran_sear.fit(X, y)
+print("Best Accuracy : " + str(ran_sear.best_score_))
+print("Best Params : " + str(ran_sear.best_params_))
