91288f7c889748aaedb73f8587d9bf1e4e639c1a
Saketh Katari <katari15045@iiitd.ac.in>
2018-08-16 00:35:40 +0530

    misc : adds gridSearchCV on SVM

---
+++ misc/4_tuning_params/3_grid_search/2_svm/main.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+
+# Load Data
+iris = load_iris()
+X = iris.data
+y = iris.target
+print("Raw Data : " + str(X.shape) + " | " + str(y.shape))
+
+# Make Parametres
+params = {}
+params["C"] = [2**(-5), 2**(-3), 2**(-1), 2**1, 2**3, 2**5, 2**7, 2**9, 2**11, 2**13, 2**15]
+params["gamma"] = [2**(-15), 2**(-13), 2**(-11), 2**(-9), 2**(-7), 2**(-5), 2**(-3), 2**(-1), 2**1, 2**3]
+params["kernel"] = ["linear", "poly", "rbf", "sigmoid"]
+
+# Perform Grid Search
+model = SVC()
+grid = GridSearchCV(model, params, cv=10, scoring="accuracy", n_jobs=-1, verbose=1)
+grid.fit(X, y)
+print("Best Accuracy : " + str(grid.best_score_))
+print("Best Params : " + str(grid.best_params_))
+
+
+
