a471ec360cf3526d774b23d1c8c73e8a8398b7ab
Saketh Katari <katari15045@iiitd.ac.in>
2018-12-04 00:30:27 +0530

    ass_4: final submission

--- course/ass_4/README.txt
+++ course/ass_4/README.txt
@@ -1,2 +1,4 @@
-To run the program, 
+1. To run the program, 
 	python3 main.py
+
+2. Documentation: https://docs.google.com/document/d/12SaHD5i03tvRzk7x8hufkBgAxi-_zlM5cC0qe5QM9nY/edit?usp=sharing
--- course/ass_4/main.py
+++ course/ass_4/main.py
@@ -2,21 +2,33 @@ import gym
 import gym_maze
 from gym import envs
 from random import randint
+from random import random
 
 class Main:
 	
-	games = 1
+	gamma = 0.3 #discount rate
+	learning_rate = 0.3
+	alpha = 0 #exploration rate
+	games = 20
 	steps = 300
-	gamma = 0.3
 	actions = 4
 	goal_state = [4, 4]
 	manhattan_dists_file = "manhattan_dists.csv"
+	exploration_rate_analysis_file = "exploration_rate_analysis.csv"
+	train_games_analysis_file = "train_games_analysis.csv"
 
 	@staticmethod
 	def main():
+		Main.train()
+		#Main.exploration_rate_analysis()
+
+	@staticmethod
+	def q_learning(display):
 		# pre processing
 		Main.q_data = dict()
 		Main.manhattan_dists = []
+		games_steps = []
+		sum_steps = 0
 		# start
 		env = gym.make("maze-sample-5x5-v0")
 		count_games = 1
@@ -27,8 +39,9 @@ class Main:
 			new_state[1] = int(new_state[1])
 			#while(count_steps <= Main.steps):
 			while(0 != 1):
-				Main.print_q_data(new_state, before=True)
-				env.render()
+				#Main.print_q_data(new_state, before=True)
+				if(display == True):
+					env.render()
 				#action = env.action_space.sample()
 				#action = Main.random_action(new_state)
 				action = Main.best_action(new_state)
@@ -37,18 +50,22 @@ class Main:
 				for num in new_state:
 					old_state.append(num)
 				new_state, reward, game_over, log = env.step(action)
-				print("action: " + str(action))
-				print("new_state: " + str(new_state))
+				#print("action: " + str(action))
+				#print("new_state: " + str(new_state))
 				Main.update_q_data(old_state, action, reward, new_state)
-				Main.print_q_data(new_state, before=False)
+				#Main.print_q_data(new_state, before=False)
 				if(game_over):
 					Main.manhattan_dists.append(Main.manhattan_dist(new_state))
+					games_steps.append(count_steps)
+					sum_steps = sum_steps + count_steps
 					print("Game " + str(count_games) + " Over! In " + str(count_steps) + " steps.")
 					break
 				count_steps = count_steps+1
 			count_games = count_games+1
+		avg_steps = sum_steps/Main.games
 		env.close()
-		Main.save_manhattan_dists()
+		#Main.save_manhattan_dists()
+		return avg_steps, games_steps
 
 	@staticmethod
 	def best_action(state):
@@ -56,8 +73,11 @@ class Main:
 		if(Main.q_data.get(state) == None):
 			Main.add_state_to_q_data(state)
 			return randint(0, Main.actions-1)
-		else:
-			return Main.action_with_max_q(state)
+		# Considering exploration rate, generate a random number in range [0, 1)
+		rand = random()
+		if(rand <= Main.alpha):		
+			return randint(0, Main.actions-1)
+		return Main.action_with_max_q(state)
 	
 	@staticmethod
 	def random_action(state):
@@ -74,7 +94,31 @@ class Main:
 			Main.add_state_to_q_data(new_state)
 		max_q_action = Main.action_with_max_q(new_state)
 		max_q = Main.q_data[new_state][max_q_action]
-		Main.q_data[old_state][action] = reward + (Main.gamma * max_q)		
+		Main.q_data[old_state][action] = Main.learning_rate*(reward + (Main.gamma * max_q))
+
+	@staticmethod
+	def train():
+		book = open(Main.train_games_analysis_file, "w")
+		book.write("ith Game,Steps\n")
+		_, games_steps = Main.q_learning(False)
+		count = 1
+		while(count <= len(games_steps)):
+			cur_steps = games_steps[count-1]
+			book.write(str(count) + "," + str(cur_steps) + "\n")
+			count = count+1
+		book.close()
+
+	@staticmethod
+	def exploration_rate_analysis():
+		Main.alpha = 0
+		avg_steps_arr = []
+		book = open(Main.exploration_rate_analysis_file, "w")
+		book.write("Exploration rate (alpha),Average steps taken (10 samples)\n")
+		while(Main.alpha <= 1):
+			avg_steps, _ = Main.q_learning(False)
+			book.write(str(Main.alpha) + "," + str(avg_steps) + "\n")
+			Main.alpha = Main.alpha+0.1
+		book.close()
 
 	@staticmethod
 	def manhattan_dist(cur_state):
