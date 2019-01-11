94dbcbecf35d38cfeadeb7b3bdb7812447f2e2fe
Saketh Katari <katari15045@iiitd.ac.in>
2019-01-11 23:22:38 +0530

    ass_4: removes redundant code

--- course/ass_4/code/main.py
+++ course/ass_4/code/main.py
@@ -4,6 +4,7 @@ import gym_maze
 from gym import envs
 from random import randint
 from random import random
+from matplotlib import pyplot
 
 class env_state:
 	q_value = 0
@@ -15,86 +16,38 @@ class Main:
 	learning_rate = 0.9
 	alpha = 0.9 #exploration rate
 	games = 20
-	steps = 300
 	actions = 4
-	goal_state = [4, 4]
-	state_1 = "2,2"
-	state_2 = "3,3"
-	manhattan_dists_file = "manhattan_dists.csv"
-	q_data_file = "q_data.pkl"
 
 	@staticmethod
 	def main():
-		# Training the model
-		rewards_sum, games_steps, alphas, action_arr_1, action_arr_2 = Main.q_learning(False, "maze-sample-5x5-v0", False)
-		Main.state_analysis(action_arr_1, action_arr_2)
-		avg_steps = sum(games_steps)/len(games_steps)
-		print("===========================================================================")
-		print("Sum of Rewards in the last game to reach goal state: " + str(rewards_sum))
-		print("Steps taken to reach goal state: " + str(games_steps))
-		print("Average steps taken: " + str(avg_steps))
-		print("Exploration Rate in each game: " + str(alphas))
-
-		# testing on a new grid
-		Main.q_data = Main.load_pickle(Main.q_data_file)
-		Main.q_learning(False, "maze-random-5x5-v0", True)
-		#Main.display_available_envs()
-
+		Main.q_data = dict()
+		Main.q_learning("maze-sample-5x5-v0")
+	
 	@staticmethod
-	def q_learning(display, maze_name, test):
-		# pre processing
-		if(test == False):
-				Main.q_data = dict()
-		else:
-			Main.games = 1
-		Main.manhattan_dists = []
+	def q_learning(maze_name):
 		games_steps = []
-		alphas = []
-		action_arr_1_2d = []
-		action_arr_2_2d = []
-		# start
 		env = gym.make(maze_name)
 		count_games = 1
 		while(count_games <= Main.games):
-			#print("(alpha, gamma, learning_rate) : " + str(Main.alpha) + ", " + str(Main.gamma) + ", " + str(Main.learning_rate))
-			alphas.append(round(Main.alpha, 3))
 			count_steps = 1
-			rewards_sum = 0
-			action_arr_1 = []	
-			action_arr_2 = []
 			new_state = env.reset()
 			new_state[0] = int(new_state[0])
 			new_state[1] = int(new_state[1])
-			#while(count_steps <= Main.steps):
 			while(0 != 1):
-				#Main.print_q_data(new_state, before=True)
-				if(display == True):
+				if(count_games == Main.games):
 					env.render()
-				#action = env.action_space.sample()
-				#action = Main.random_action(new_state)
+					filename = str(count_steps) + ".png"
+					pyplot.imshow(env.render())
+					pyplot.savefig(filename)
 				action = Main.best_action(new_state)	
-				# Analysing 2 states
 				new_state_str = Main.state_to_str(new_state)
-				if(new_state_str == Main.state_1):
-					action_arr_1.append(action)
-				elif(new_state_str == Main.state_2):
-					action_arr_2.append(action)
 				old_state = []
 				for num in new_state:
 					old_state.append(num)
 				new_state, reward, game_over, log = env.step(action)
-				if(count_games == Main.games):
-					if(count_steps == 1):
-						print("Reward: " + str(reward))
-					Main.manhattan_dists.append(Main.manhattan_dist(new_state))
-				rewards_sum = rewards_sum+reward
 				Main.q_data[Main.state_to_str(old_state)][action].reward = reward
-				#print("action: " + str(action))
-				#print("new_state: " + str(new_state))
 				Main.update_q_data(old_state, action, reward, new_state)
-				#Main.print_q_data(new_state, before=False)
 				if(game_over):
-					games_steps.append(count_steps)
 					if(count_steps == 2000):
 						print("Couldn't reach goal state!")
 					else:
@@ -103,13 +56,7 @@ class Main:
 				count_steps = count_steps+1
 			count_games = count_games+1
 			Main.alpha = Main.alpha*0.30
-			action_arr_1_2d.append(action_arr_1)
-			action_arr_2_2d.append(action_arr_2)
 		env.close()
-		Main.save_manhattan_dists()
-		if(test == False):
-			Main.dump_pickle(Main.q_data, Main.q_data_file)
-		return rewards_sum, games_steps, alphas, action_arr_1_2d, action_arr_2_2d
 
 	@staticmethod
 	def best_action(state):
@@ -124,13 +71,6 @@ class Main:
 		return Main.action_with_max_q(state)
 	
 	@staticmethod
-	def random_action(state):
-		state = Main.state_to_str(state)
-		if(Main.q_data.get(state) == None):
-			Main.add_state_to_q_data(state)
-		return randint(0, Main.actions-1)
-
-	@staticmethod
 	def update_q_data(old_state, action, reward, new_state):
 		old_state = Main.state_to_str(old_state)
 		new_state = Main.state_to_str(new_state)
@@ -139,12 +79,6 @@ class Main:
 		max_q_action = Main.action_with_max_q(new_state)
 		max_q = Main.q_data[new_state][max_q_action].q_value
 		Main.q_data[old_state][action].q_value = Main.learning_rate*(reward + (Main.gamma * max_q))
-
-	@staticmethod
-	def manhattan_dist(cur_state):
-		x_dist = abs(cur_state[0] - Main.goal_state[0])
-		y_dist = abs(cur_state[1] - Main.goal_state[1])
-		return x_dist + y_dist	
 
 	# Given a state, it returns an action with max q value
 	@staticmethod
@@ -191,56 +125,5 @@ class Main:
 			print(key_ + ": " + str(value_))
 		if(before == False):
 			print("====================================")
-
-	@staticmethod
-	def save_manhattan_dists():
-		book = open(Main.manhattan_dists_file, "w")
-		book.write("Step (Just before step k),Manhattan Distance\n")
-		ind = 0
-		while(ind < len(Main.manhattan_dists)):
-			manhattan_dist_ = Main.manhattan_dists[ind]
-			book.write(str(ind+1) + "," + str(manhattan_dist_) + "\n")
-			ind = ind+1
-		book.close()
-
-	@staticmethod
-	def display_available_envs():
-		list_ = envs.registry.all()
-		for obj in list_:
-			print(obj)
-
-	@staticmethod
-	def state_analysis(actions_1, actions_2):
-		Main.write_to_csv(actions_1, "state_analysis_1.csv", Main.state_1)
-		Main.write_to_csv(actions_2, "state_analysis_2.csv", Main.state_2)
-
-	@staticmethod
-	def write_to_csv(data, filename, state):
-		book = open(filename, "w")
-		state_arr = state.split(",")
-		row = state_arr[0]
-		col = state_arr[1]
-		book.write("Actions selected at state " + str(row) + "|" + str(col) + "\n")
-		ind = 0
-		while(ind < len(data)):
-			action = data[ind]
-			action = str(action)
-			action = action.replace(",", "|")
-			book.write(str(action) + "\n")
-			ind = ind+1
-		book.close()
-
-	@staticmethod
-	def dump_pickle(data, filename):
-		book = open(filename, "wb")
-		pickle.dump(data, book)
-		book.close()
-
-	@staticmethod
-	def load_pickle(filename):
-		book = open(filename, "rb")
-		data = pickle.load(book)
-		book.close()
-		return data
 
 Main.main()
